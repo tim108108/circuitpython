584e9232f14af9cffea2fb4a967367b8e41ba6c8
tim108108 <36764374+tim108108@users.noreply.github.com>
2022-04-28 22:46:04 +0800

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,41 @@ import board
 import pwmio
 import time
 
+a1 = digitalio.DigitalInOut(board.IO36);a1.direction = digitalio.Direction.OUTPUT;
+b1 = digitalio.DigitalInOut(board.IO38);b1.direction = digitalio.Direction.OUTPUT;
+a2 = digitalio.DigitalInOut(board.IO37);a2.direction = digitalio.Direction.OUTPUT;
+b2 = digitalio.DigitalInOut(board.IO39);b2.direction = digitalio.Direction.OUTPUT;
+
+def wave_drive():
+    wave_table = [1,0,0,0]
+    for i  in range(4):
+        a1.value = wave_table[i]
+        b1.value = wave_table[(i+3)%4]
+        a2.value = wave_table[(i+2)%4]
+        b2.value = wave_table[(i+1)%4]
+        time.sleep(0.1)
+#         print(wave_table[i],wave_table[(i+3)%4],wave_table[(i+2)%4],wave_table[(i+1)%4])
+
+def full_step_drive():
+    full_step_table = [1,0,0,1]
+    for i  in range(4):
+        a1.value = full_step_table[i]
+        b1.value = full_step_table[(i+3)%4]
+        a2.value = full_step_table[(i+2)%4]
+        b2.value = full_step_table[(i+1)%4]
+        time.sleep(0.1)
+#         print(full_step_table[i], full_step_table[(i+3)%4], full_step_table[(i+2)%4], full_step_table[(i+1)%4])
+
+def half_step_drive():
+    half_step_table = [[1,0,0,1],[1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,1,1],[0,0,0,1]]
+    for i  in range(8):
+        a1.value = half_step_table[i][0]
+        b1.value = half_step_table[i][1]
+        a2.value = half_step_table[i][2]
+        b2.value = half_step_table[i][3]
+        time.sleep(0.1)
+#         print(half_step_table[i][0], half_step_table[i][1], half_step_table[i][2], half_step_table[i][3])
+    
 in1 = pwmio.PWMOut(board.IO11, frequency=5000, duty_cycle=0)
 in2 = pwmio.PWMOut(board.IO12, frequency=5000, duty_cycle=0)
 in3 = pwmio.PWMOut(board.IO13, frequency=5000, duty_cycle=0)
@@ -26,4 +61,10 @@ def spwm():
 #         print(int(65535*spwm_table[i]),int(65535*spwm_table[(i+4)%12]),int(65535*spwm_table[(i+8)%12]))
 
 while True:
-    spwm()
+#     wave_drive()
+#     full_step_drive()
+#     half_step_drive()
+#     spwm()
+     
+
+
