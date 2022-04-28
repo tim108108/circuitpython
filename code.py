ac459c639662b0dc62c981701bea1e929f06dcc6
tim108108 <36764374+tim108108@users.noreply.github.com>
2022-04-28 21:27:39 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,14 +1,16 @@
 import digitalio
+import analogio
 import board
 import pwmio
 import time
 
-
-in1 = pwmio.PWMOut(board.IO37, frequency=5000, duty_cycle=0)
-in2 = pwmio.PWMOut(board.IO38, frequency=5000, duty_cycle=0)
-in3 = pwmio.PWMOut(board.IO39, frequency=5000, duty_cycle=0)
-en = digitalio.DigitalInOut(board.IO40)
+in1 = pwmio.PWMOut(board.IO11, frequency=5000, duty_cycle=0)
+in2 = pwmio.PWMOut(board.IO12, frequency=5000, duty_cycle=0)
+in3 = pwmio.PWMOut(board.IO13, frequency=5000, duty_cycle=0)
+en = digitalio.DigitalInOut(board.IO14)
 en.direction = digitalio.Direction.OUTPUT
+i2 = analogio.AnalogIn(board.IO15)
+i3 = analogio.AnalogIn(board.IO16)
 
 def spwm():
     spwm_table=[0.5,0.75,0.933,1.0,0.933,0.75,0.5,0.25,0.066,0.0,0.066,0.25]
@@ -17,7 +19,8 @@ def spwm():
         in1.duty_cycle = int(65535*spwm_table[i])
         in2.duty_cycle = int(65535*spwm_table[(i+4)%12])
         in3.duty_cycle = int(65535*spwm_table[(i+8)%12])
-        time.sleep(0.1)
+#         time.sleep(0.01)
+#         print(i2.value/ 65535*i2.reference_voltage,i3.value/ 65535*i3.reference_voltage)
 #         print(i,(i+4)%12,(i+8)%12)
 #         print(spwm_table[i],spwm_table[(i+4)%12],spwm_table[(i+8)%12])
 #         print(int(65535*spwm_table[i]),int(65535*spwm_table[(i+4)%12]),int(65535*spwm_table[(i+8)%12]))
